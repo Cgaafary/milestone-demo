675fd939d5ecc60fb38320f40702b316457b092b
Chris Gaafary <cgaafary@gmail.com>
2017-05-06 23:22:24 -0400

    Removed mdl styling

--- src/components/protected/FacultyComponents/MilestoneCard.js
+++ src/components/protected/FacultyComponents/MilestoneCard.js
@@ -2,32 +2,29 @@ import React from 'react';
 
 export default function MilestoneCard(props) {
     const { description, level } = props;
+
     function handleYesResponse() {
         const { id } = props;
         props.handleMilestoneResponse({milestone: id, achieved: true,});
     }
 
     function handleNoResponse() {
-        const { id } = this.props;
+        const { id } = props;
         props.handleMilestoneResponse({milestone: id, achieved: false});
     }
 
     return (
-        <div className="mdl-cell mdl-cell--4-col mdl-card mdl-shadow--2dp milestone-card">
+        <div>
         <p><strong>Level { level }</strong></p>
         <p>{description}</p>
-        <div className="milestone-button">
+        <div>
             <button
-                className="mdl-button mdl-js-button mdl-button--primary"
                 onClick={handleNoResponse}
             >No</button>
             <button
-                className="mdl-button mdl-js-button mdl-button--primary"
                 onClick={handleYesResponse}
             >Yes</button>
         </div>
         </div>
     );
 }
-
-// export default MilestoneCard;
